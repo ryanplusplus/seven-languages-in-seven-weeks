0d89678c34ac2a04acdb292ea1171ef1a8cc53c4
Ryan Hartlage <ryanplusplus@gmail.com>
2015-11-08 08:31:21 -0500

    Everything but the bonus problem

---
+++ ruby/day1/do.rb
@@ -0,0 +1,17 @@
+# Print the string "Hello, world."
+puts 'Hello, world'
+
+# For the string "Hello, Ruby," find the index of the word "Ruby."
+puts 'hello, Ruby'.index('Ruby')
+
+# print your name ten times.
+10.times { puts 'Ryan' }
+
+# Print he string "This is sentence number 1," where the number 1 ranges from 1 to 10.
+10.times { |i| puts "This is sentence number #{i}" }
+
+# Run a Ruby program from a file.
+# ruby do.rb
+
+# Bonus problem: if you're feeling the need for a little more, write a program that picks a random number.
+# Let a player guess the number, telling the player whether the guess is too low or too high.
