d1eff7d57309310dc1dd5187c222de100440a14c
Ryan Hartlage <ryanplusplus@gmail.com>
2016-03-19 20:50:06 -0400

    Erlang day 1: word count

---
+++ erlang/day1/word_count.erl
@@ -0,0 +1,13 @@
+% Write a function that uses recursion to return the number of words in a string.
+
+-module(word_count).
+-export([word_count/1]).
+
+aux([], Acc) -> Acc;
+aux([$ ], Acc) -> Acc;
+aux([$ , $ ], Acc) -> Acc;
+aux([$ , $  | Tail], Acc) -> aux(" " ++ Tail, Acc);
+aux([$  | Tail], Acc) -> aux(Tail, Acc + 1);
+aux([_ | Tail], Acc) -> aux(Tail, Acc).
+
+word_count(String) -> aux(" " ++ String, 0).
