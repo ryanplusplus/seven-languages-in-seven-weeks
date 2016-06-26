4c79eb3f5a53fef373bacafdc68777bf2347d4ab
Ryan Hartlage <ryanplusplus@gmail.com>
2016-06-26 19:14:24 -0400

    Haskell day 2: partial application exercises

---
+++ haskell/day2/partial.hs
@@ -0,0 +1,6 @@
+-- Use a partially applied function to define a function that will return half
+-- of a number and another that will append \n to the end of any string.
+
+module Main where
+  half x = (/ 2)
+  addNewline s = (++ "\n")
